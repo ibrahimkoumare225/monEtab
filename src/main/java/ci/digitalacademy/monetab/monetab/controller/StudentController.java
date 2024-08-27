caedc7c17e54426e691bf9219b04187263aa5cc6
Koumare <ibrahimkoumare5@gmail.com>
2024-08-27 14:04:41 +0100

    finalisation des insertions de la page web pour Eleve

--- src/main/java/ci/digitalacademy/monetab/monetab/controller/StudentController.java
+++ src/main/java/ci/digitalacademy/monetab/monetab/controller/StudentController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,13 +24,20 @@ public class StudentController {
         model.addAttribute("students",students);
         return "dynamic/eleves/homeEleve";
     }
+    @PostMapping("/homeEleve")
+    public String saveStudent(Student student){
+        studentService.save(student);
+        return "redirect:/homeEleve";
+    }
     @GetMapping("/ajouterEleve")
-    public String addStudent(){
+    public String addStudent(Model model){
+        model.addAttribute("student",new Student());
         return "dynamic/eleves/ajouterEleve";
     }
 
     @GetMapping("/updateEleve")
     public String updateStudent(){
+
         return "dynamic/eleves/updateEleve";
     }
 
