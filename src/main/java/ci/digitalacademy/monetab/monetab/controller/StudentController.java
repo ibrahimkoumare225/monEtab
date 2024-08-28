0be8d997bb67ab2ba2e388c2ada351249088f122
Koumare <ibrahimkoumare5@gmail.com>
2024-08-28 14:26:16 +0100

    Implémentation de la methode delete pour eleves

--- src/main/java/ci/digitalacademy/monetab/monetab/controller/StudentController.java
+++ src/main/java/ci/digitalacademy/monetab/monetab/controller/StudentController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
@@ -54,6 +55,14 @@ public class StudentController {
         } else {
             return "redirect:/homeEleve";
         }
+    }
+
+    @PostMapping("/deleteStudent/{id}")
+    public String deleteStudent(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        log.info("Deleting student with id: " + id);
+        studentService.delete(id);
+        redirectAttributes.addFlashAttribute("message", "Student deleted successfully!");
+        return "redirect:/homeEleve";
     }
 
 }
