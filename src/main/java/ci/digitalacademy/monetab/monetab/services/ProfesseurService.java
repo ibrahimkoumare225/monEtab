18266cc6e0aa9fbe9136984f6372cab0553489f6
Koumare <ibrahimkoumare5@gmail.com>
2024-08-28 23:42:54 +0100

    Mise a jour de l'interface Professeur

--- src/main/java/ci/digitalacademy/monetab/monetab/services/ProfesseurService.java
+++ src/main/java/ci/digitalacademy/monetab/monetab/services/ProfesseurService.java
@@ -1,17 +1,18 @@
 package ci.digitalacademy.monetab.monetab.services;
 
 import ci.digitalacademy.monetab.monetab.models.Professeur;
+import ci.digitalacademy.monetab.monetab.services.dto.ProfesseurDTO;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ProfesseurService {
 
-    Professeur save(Professeur professeur);
-    Professeur update(Professeur professeur);
+    ProfesseurDTO save(ProfesseurDTO professeurDTO);
+    ProfesseurDTO update(ProfesseurDTO professeurDTO);
 
-    Optional<Professeur> findOne(Long id);
+    Optional<ProfesseurDTO> findOne(Long id);
 
-    List<Professeur> findAll();
+    List<ProfesseurDTO> findAll();
     void delete(Long id);
 }
