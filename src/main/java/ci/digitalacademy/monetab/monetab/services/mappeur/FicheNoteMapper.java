92b0433a9e6d50bee7f4ad2149a707fd7253da63
Koumare <ibrahimkoumare5@gmail.com>
2024-08-28 15:37:28 +0100

    Implémentation de FicheNoteMapper

--- src/main/java/ci/digitalacademy/monetab/monetab/services/mappeur/FicheNoteMapper.java
+++ src/main/java/ci/digitalacademy/monetab/monetab/services/mappeur/FicheNoteMapper.java
@@ -3,12 +3,23 @@ package ci.digitalacademy.monetab.monetab.services.mappeur;
 import ci.digitalacademy.monetab.monetab.models.FicheNote;
 import ci.digitalacademy.monetab.monetab.services.dto.FicheNoteDTO;
 
-public class FicheNoteMapper {
+public final class FicheNoteMapper {
 
     private FicheNoteMapper(){}
 
+
+    public  static FicheNoteDTO toDto(FicheNote ficheNote){
+        FicheNoteDTO ficheNoteDTO = new FicheNoteDTO();
+        ficheNoteDTO.setId(ficheNoteDTO.getId());
+        ficheNoteDTO.setNote(ficheNote.getNote());
+        ficheNoteDTO.setAnnee(ficheNote.getAnnee());
+        return ficheNoteDTO;
+    }
     public static FicheNote toEntity(FicheNoteDTO ficheNoteDTO){
         FicheNote ficheNote = new FicheNote();
+        ficheNote.setId(ficheNoteDTO.getId());
+        ficheNote.setNote(ficheNoteDTO.getNote());
+        ficheNote.setAnnee(ficheNoteDTO.getAnnee());
         return ficheNote;
     }
 }
