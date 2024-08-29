71cc9054ca40e9ab63851adfaad91a38dcff8842
Koumare <ibrahimkoumare5@gmail.com>
2024-08-29 21:57:46 +0100

    Création de du dto School

---
+++ src/main/java/ci/digitalacademy/monetab/monetab/services/dto/SchoolDTO.java
@@ -0,0 +1,13 @@
+package ci.digitalacademy.monetab.monetab.services.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SchoolDTO {
+
+    private Long id;
+    private String name;
+    private String url_logo;
+}
